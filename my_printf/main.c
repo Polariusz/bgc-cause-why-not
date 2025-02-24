3c8703773ac0350649c5807dfd08b772be64b5c2
Polariusz <truepolariusz@gmail.com>
2025-02-24 17:33:43 +0100

    macro magic

---
+++ my_printf/main.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdarg.h>
+
+int my_printf(const char *format, ...)
+{
+    va_list va;
+
+    va_start(va, format);
+    int rv = vprintf(format, va);
+    va_end(va);
+    return rv;
+}
+
+int main(void)
+{
+    int foo = 42;
+    double bar = 69.0;
+    my_printf("x is %d, y is %lf\n", foo, bar);
+    printf("Just Monika.\n");
+    return 0;
+}
