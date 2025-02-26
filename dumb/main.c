68a78c203e4cf1ba81a24945f338625458e5758a
Polariusz <truepolariusz@gmail.com>
2025-02-26 08:29:48 +0100

    strlen of an euro sign character returns more than one byte!

--- dumb/main.c
+++ dumb/main.c
@@ -1,4 +1,6 @@
 #include <unistd.h>
+#include <string.h>
+#include <stdio.h>
 
 void my_print(char *t)
 {
@@ -11,7 +13,10 @@ void my_print(char *t)
 
 int main(void)
 {
+    char *txt = "1,99€";
     my_print("1,99€\n");
+    printf("%s, len: %ld\n", txt, strlen(txt));
+    // > 1,99€, len: 7
 
     my_print("Just Monika.\n");
     return 0;
