a9bf1b9171d833f5347ef125683e0420a35dc8cf
Polariusz <truepolariusz@gmail.com>
2025-02-26 15:41:34 +0100

    played a bit around signals, but I decided to remove it because it will be nicer to have a new directory for that

--- dumb/main.c
+++ dumb/main.c
@@ -1,23 +1,7 @@
-#include <unistd.h>
-#include <string.h>
 #include <stdio.h>
-
-void my_print(char *t)
-{
-    char *p = t;
-    while (*p != '\0') {
-	write(1, p, 1);
-	++p;
-    }
-}
 
 int main(void)
 {
-    char *txt = "1,99€";
-    my_print("1,99€\n");
-    printf("%s, len: %ld\n", txt, strlen(txt));
-    // > 1,99€, len: 7
-
-    my_print("Just Monika.\n");
+    printf("Just Monika.\n");
     return 0;
 }
